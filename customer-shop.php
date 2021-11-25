dc573825283ae49d9ac4040b5af4c1374f877be6
friedchickean <47467633+friedchickean@users.noreply.github.com>
2021-11-24 20:52:52 -0700

    added checkout button and total placeholder

--- customer-shop.php
+++ customer-shop.php
@@ -26,14 +26,36 @@
 </div>
 
 <div id="shop-container">
+    <h1>Items</h1>
     <div class="item-grid">
-        <div class="item1"><input type=image src="images/item.png"></div>
-        <div class="item2"><input type=image src="images/item.png"></div>
-        <div class="item3"><input type=image src="images/item.png"></div>
-        <div class="item4"><input type=image src="images/item.png"></div>
-        <div class="item5"><input type=image src="images/item.png"></div>
-        <div class="item6"><input type=image src="images/item.png"></div>
+        <div class="item1">
+            <input type=image src="images/item.png">
+            <p>Item 1       $4.99</p>
+        </div>
+        <div class="item2">
+            <input type=image src="images/item.png">
+            <p>Item 2       $6.99</p>
+        </div>
+        <div class="item3">
+            <input type=image src="images/item.png">
+            <p>Item 3       $14.99</p>
+        </div>
+        <div class="item4">
+            <input type=image src="images/item.png">
+            <p>Item 4       $1.99</p>
+        </div>
+        <div class="item5">
+            <input type=image src="images/item.png">
+            <p>Item 5       $24.99</p>
+        </div>
+        <div class="item6">
+            <input type=image src="images/item.png">
+            <p>Item 5       $17.99</p>
+        </div>
     </div>
+
+    <button type="button">Proceed to checkout</button>
+    <p id="total">Total:    $39.64</p>
 </div>
 
 </body>
